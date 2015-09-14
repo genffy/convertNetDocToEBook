5f674ec4dc12215c389419b8d7601ba57ef6e22e
madao <lee.genffy@gmail.com>
2015-09-14 16:14:54 +0800

    批量抓取

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
 var http = require("http"),
     https = require("https"),
     fs = require("fs"),
-    path = require("path");
+    path = require("path"),
+    $ = require("jquery")(require("jsdom").jsdom().parentWindow);
 
 var baseUrl = "laravel.com",
     basePath = "/docs/5.1",
@@ -16,6 +17,43 @@ var opt = {
     path: basePath
 };
 
+/**
+ * 根据右边的导航获取URL列表
+ */
+function getUrlList(html){
+    var $dom = $(html), urls = [];
+    $dom.find(">ul>li").each(function(){
+        var _$li = $(this), title = _$li.text();
+        $(this).find("a").each(function(){
+            var _$a = $(this), __obj = {
+                title: title,
+                subTitle: _$a.text(),
+                host: baseUrl,
+                path: _$a.attr("href")
+            };
+            urls.push(__obj);
+        });
+    });
+    return urls;
+}
+/**
+ * 保存获取到的内容
+ */
+function saveContent(opt, content){
+    var filename = JSON.stringify(/<title>.*<\/title>/gi.exec(content)[0]).replace(/<\/*title>/g,'')+".txt";
+    fs.stat(htmlSrc+opt.host +"/" +filename, function(err, stat){
+        if(err == null) {
+            console.log('File exists');
+        } else if(err.code == 'ENOENT') {
+            fs.writeFile(htmlSrc+opt.host +"/" +filename, content, function(err){
+                if(err) throw err;
+                console.log("It's saved!");
+            });
+        } else {
+            console.log('Some other error: ', err.code);
+        }
+    });
+}
 function callback(response){
     var str = "";
 
@@ -24,42 +62,40 @@ function callback(response){
     });
 
     response.on("end", function(){
+        paths = getUrlList(str);
         // 写到html原始文档里
         var stat = fs.statSync(htmlSrc+opt.host);
+
+        for(var i=0, len = paths.length; i<len; i++){
+            (function(opt){
+                http.get(opt, function(res){
+                    res.on("data", function(){
+
+                    });
+                    res.on("end", function(){
+                        if(stat.isDirectory()){
+                            saveContent(opt, str);
+                        }else{
+                            fs.mkdir(htmlSrc+opt.host, 0755, function(err){
+                                if(err) throw err;
+                                saveContent(opt, str);
+                            });
+                        }
+                    });
+                });
+            })(paths[i]);
+        }
         if(stat.isDirectory()){
             var filename = JSON.stringify(/<title>.*<\/title>/gi.exec(str)[0]).replace(/<\/*title>/g,'')+".txt";
-            fs.stat(htmlSrc+opt.host +"/" +filename, function(err, stat){
-                if(err == null) {
-                    console.log('File exists');
-                } else if(err.code == 'ENOENT') {
-                    fs.writeFile(htmlSrc+opt.host +"/" +filename, str, function(err){
-                        if(err) throw err;
-                        console.log("It's saved!");
-                    });
-                } else {
-                    console.log('Some other error: ', err.code);
-                }
-            });
+            saveContent(opt, str);
         }else{
             fs.mkdir(htmlSrc+opt.host, 0755, function(err){
                 if(err) throw err;
-                var filename = JSON.stringify(/<title>.*<\/title>/gi.exec(str)[0]).replace(/<\/*title>/g,'')+".txt";
-                fs.stat(htmlSrc+opt.host +"/" +filename, function(err, stat){
-                    if(err == null) {
-                        console.log('File exists');
-                    } else if(err.code == 'ENOENT') {
-                        fs.writeFile(htmlSrc+opt.host +"/" +filename, str, function(err){
-                            if(err) throw err;
-                            console.log("It's saved!");
-                        });
-                    } else {
-                        console.log('Some other error: ', err.code);
-                    }
-                });
+                saveContent(opt, str);
             });
         }
     });
-};
+}
 
 var request = http.get(opt, callback);
 request.on("error", function (error) {
