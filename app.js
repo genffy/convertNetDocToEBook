93f2ba1d2de4178d8a4bc8f971f6965986523579
genffy <genffy@163.com>
2015-09-14 23:03:10 +0800

    add phantomjs convert html to pdf but not working

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var http = require("http"),
     https = require("https"),
     fs = require("fs"),
     path = require("path"),
+    phantom = require("phantom"),
     $ = require("jquery")(require("jsdom").jsdom().parentWindow);
 
 var baseUrl = "laravel.com",
@@ -21,7 +22,7 @@ var opt = {
  * 根据右边的导航获取URL列表
  */
 function getUrlList(html){
-    var $dom = $(html), urls = [];
+    var $dom = $(html).find(".sidebar"), urls = [];
     $dom.find(">ul>li").each(function(){
         var _$li = $(this), title = _$li.text();
         $(this).find("a").each(function(){
@@ -29,7 +30,8 @@ function getUrlList(html){
                 title: title,
                 subTitle: _$a.text(),
                 host: baseUrl,
-                path: _$a.attr("href")
+                path: _$a.attr("href"),
+                status:0
             };
             urls.push(__obj);
         });
@@ -40,13 +42,17 @@ function getUrlList(html){
  * 保存获取到的内容
  */
 function saveContent(opt, content){
-    var filename = JSON.stringify(/<title>.*<\/title>/gi.exec(content)[0]).replace(/<\/*title>/g,'')+".txt";
-    fs.stat(htmlSrc+opt.host +"/" +filename, function(err, stat){
+    var filename = JSON.stringify(/<title>.*<\/title>/gi.exec(content)[0])
+            .replace(/<\/*title>/g,'')
+            .replace("\"","")
+            .replace("\/","_")
+            .replace(" ","_").split('-')[0]+".txt";
+    fs.stat(htmlSrc+baseUrl +"/" +filename, function(err, stat){
         if(err == null) {
             console.log('File exists');
         } else if(err.code == 'ENOENT') {
-            fs.writeFile(htmlSrc+opt.host +"/" +filename, content, function(err){
-                if(err) throw err;
+            fs.writeFile(htmlSrc+baseUrl +"/" +filename, content, function(err){
+                if(err) console.log(err);
                 console.log("It's saved!");
             });
         } else {
@@ -64,36 +70,29 @@ function callback(response){
     response.on("end", function(){
         paths = getUrlList(str);
         // 写到html原始文档里
-        var stat = fs.statSync(htmlSrc+opt.host);
-
-        for(var i=0, len = paths.length; i<len; i++){
-            (function(opt){
-                http.get(opt, function(res){
-                    res.on("data", function(){
-
-                    });
-                    res.on("end", function(){
-                        if(stat.isDirectory()){
-                            saveContent(opt, str);
-                        }else{
-                            fs.mkdir(htmlSrc+opt.host, 0755, function(err){
-                                if(err) throw err;
-                                saveContent(opt, str);
-                            });
-                        }
-                    });
-                });
-            })(paths[i]);
-        }
+        var stat = fs.statSync(htmlSrc+baseUrl);
         if(stat.isDirectory()){
-            var filename = JSON.stringify(/<title>.*<\/title>/gi.exec(str)[0]).replace(/<\/*title>/g,'')+".txt";
             saveContent(opt, str);
         }else{
-            fs.mkdir(htmlSrc+opt.host, 0755, function(err){
+            fs.mkdir(htmlSrc+baseUrl, 0755, function(err){
                 if(err) throw err;
                 saveContent(opt, str);
             });
         }
+        console.log(paths.length);
+        //for(var i=0, len = paths.length; i<len; i++){
+        //    (function(opt){
+        //        http.get(opt, function(res){
+        //            var html = "";
+        //            res.on("data", function(chunk){
+        //                html+=chunk;
+        //            });
+        //            res.on("end", function(){
+        //                saveContent(opt, html);
+        //            });
+        //        });
+        //    })(paths[i]);
+        //}
     });
 }
 
@@ -102,3 +101,34 @@ request.on("error", function (error) {
     console.log(error);
 });
 request.end();
+
+var count = 0, interVal = setInterval(function(){
+    if(paths.length){
+        clearInterval(interVal);
+        count = paths.length
+        for(var i=0, len = paths.length; i<len; i++){
+            if(!paths[i].status){
+                (function(opt){
+                    phantom.create(function(ph){
+                        ph.createPage(function(page) {
+                            page.open(opt.host+opt.path, function(status) {
+                                if(status === "success"){
+                                    var title = page.evaluate(function() {
+                                        return document.title;
+                                    });
+                                    page.render(title+'.pdf', function(){
+                                        console.log('Page Rendered');
+                                        opt.status = 1;
+                                        ph.exit();
+                                    });
+                                }
+                            });
+                        });
+                    });
+                })(paths[i]);
+            }else{
+                continue;
+            }
+        }
+    }
+}, 1000);
